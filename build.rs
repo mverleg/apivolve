c0ffee549fe032eb1f0c612db43f31be0a3dc6ce
Mark <mverleg.noreply@gmail.com>
2021-11-14 15:22:26 +0100

    Add lalrpop for parsing migration files

---
+++ build.rs
@@ -0,0 +1,17 @@
+use ::std::env;
+use ::std::path::PathBuf;
+
+fn main() {
+    parse_grammar_definition();
+}
+
+use ::lalrpop;
+
+fn parse_grammar_definition() {
+    println!("cargo:rerun-if-changed=grammar.lalrpop");
+    let mut in_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    lalrpop::Configuration::new()
+        .set_in_dir(in_dir)
+        .process()
+        .unwrap();
+}
