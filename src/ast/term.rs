c0ffee73a51344cf6107667a10da20f6c2c5750f
Mark <mverleg.noreply@gmail.com>
2021-11-17 22:29:40 +0100

    Support numbers and nulls

--- src/ast/term.rs
+++ src/ast/term.rs
@@ -5,12 +5,20 @@ pub enum Value {
     Str(String, Span),
     Int(i64, Span),
     Real(f64, Span),
-    None,
+    None(Span),
 }
 
 impl Value {
     pub fn str(quoted_text: &str, span: Span) -> Value {
-        Value::Str(quoted_text[1 .. quoted_text.len() - 1].to_owned(), span)
+        Value::Str(quoted_text[1..quoted_text.len() - 1].to_owned(), span)
+    }
+
+    pub fn int(nr: &str, span: Span) -> Value {
+        Value::Int(nr.parse::<i64>().unwrap(), span)
+    }
+
+    pub fn real(nr: &str, span: Span) -> Value {
+        Value::Real(nr.parse::<f64>().unwrap(), span)
     }
 }
 
