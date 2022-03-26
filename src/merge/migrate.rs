9a40647920d6c08eb6ab9b1fa7553adf1b62a117
Mark <mverleg.noreply@gmail.com>
2022-03-26 22:35:35 +0100

    Introduce skeleton for application application function

---
+++ src/merge/migrate.rs
@@ -0,0 +1,6 @@
+use crate::FullEvolution;
+use crate::merge::object::State;
+
+pub fn migrate(state: &State, evolutions: &FullEvolution) -> State {
+    todo!()
+}
--- src/merge/object.rs
+++ src/merge/object.rs
@@ -1,12 +1,19 @@
+use ::ustr::Ustr;
+use ::ustr::UstrMap;
+
 use crate::ast::term::{Iden, Value};
 
 #[derive(Debug)]
 pub struct State {
-    pub objects: Vec<Object>,
+    pub objects: UstrMap<Object>,
 }
+
+/// Qualified path
+pub type ObjectId = Ustr;
 
 #[derive(Debug)]
 pub struct Object {
+    pub id: ObjectId,
     pub description: String,
     pub fields: Vec<Field>,
 }
